5e1be8e4a56d66cc8e46cf68183eec8980397288
S11013885 <70532194+S11013885@users.noreply.github.com>
2021-02-18 12:34:02 -0500

    [7b] v0.1
    created d4() and added the random function

--- dice_roll.py
+++ dice_roll.py
@@ -1,1 +1,11 @@
 # dice roll library Amari Lombel, 2/18/2021,  12:23PM, version 0.1
+
+# d4 simulator
+def roll_d4(num_roll): #num_roll is an argument.
+    import random
+    
+    rolls = 0
+    while roll <= num_roll:
+        result = random.randint(1,4)
+        print (f" you rolled a {result} on the d4!n\")
+        rolls += 1 
